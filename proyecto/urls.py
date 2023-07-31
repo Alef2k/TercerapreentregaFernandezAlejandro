ed65857287a75ff6d442af803f4fafa83ade3728
Alef2k <alef2k@hotmail.com>
2023-07-30 22:40:30 -0300

    Arreglo redireccion a pagina princial

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -3,9 +3,11 @@ URL configuration for proyecto project.
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='aplicacion/', permanent=True)),  # Redirección a la URL de la aplicación
     #
     path('aplicacion/', include('aplicacion.urls')),
 ]
